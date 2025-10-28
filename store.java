b39b7f96fde13e4c221368d4d9e9fea67d5d1cea
Abdullah Abir <abir43315@gmail.com>
2025-10-28 21:00:42 +0600

    avg.java

---
+++ store.java
@@ -0,0 +1,12 @@
+public class store {
+        public static void main(String[] arg)
+        {
+           int price=250;
+           System.out.println("The price is: " + price);
+           float discountprice;
+           discountprice = price * 0.75f;
+           System.out.println("Discount Price is: " + discountprice);
+
+
+        }
+}
