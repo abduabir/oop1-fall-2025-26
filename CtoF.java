8d8c32a586f81568b428e2150c02c33967a9f915
Abdullah Abir <abir43315@gmail.com>
2025-10-28 23:03:33 +0600

    25-62614-2

---
+++ CtoF.java
@@ -0,0 +1,10 @@
+public class CtoF {
+        public static void main(String[] args) 
+    {
+        double celsius = 23;
+        double fahrenheit;
+        fahrenheit = (celsius * 9 / 5) + 32;
+
+        System.out.println(celsius + "°C = " + fahrenheit + "°F");
+    }
+}
